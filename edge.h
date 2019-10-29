208a28ce2331019e91955ea3c5e22bf9de677bb3
carlos-reategui <carlos.reategui@utec.edu.pe>
2019-10-29 00:45:40 -0500

    main mas limpio, avance prim, mejores nombre a variables, codigo mas ordenado, refactoring, funcion readJSON

--- edge.h
+++ edge.h
@@ -1,31 +1,41 @@
-//
-// Created by Usuario on 05/10/2019.
-//
-
 #ifndef GRAFO01_EDGE_H
 #define GRAFO01_EDGE_H
 template <class U>
 class Node;
 
 template <class T>
-class Edge{
+class Edge {
     Node<T>* To;
     Node<T>* From;
     double weight;
 public:
-    Edge(){To = nullptr, From = nullptr, weight = 0;};
+    Edge() {
+        To = nullptr, From = nullptr, weight = 0;
+    };
 
-    void setTo(Node<T>* node){To = node;}
+    void setTo(Node<T>* node) {
+        To = node;
+    }
 
-    void setFrom(Node<T>* node){From = node;}
+    void setFrom(Node<T>* node) {
+        From = node;
+    }
 
-    void setWeight(double w){weight = w;}
+    void setWeight(double w) {
+        weight = w;
+    }
 
-    int getWeight(){ return weight;}
+    int getWeight() {
+        return weight;
+    }
 
-    Node<T>* getTo(){ return To;}
+    Node<T>* getTo() {
+        return To;
+    }
 
-    Node<T>* getFrom(){ return From;}
+    Node<T>* getFrom() {
+        return From;
+    }
 
     ~Edge(){
         delete To,From;
--- main.cpp
+++ main.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include "nlohmann/json.hpp"
 #include "functions.cpp"
-#include "Node.h"
+#include "node.h"
 
 using namespace std;
 using json = nlohmann::json;
 
 int main() {
-    ifstream ifs("/Users/Usuario/Documents/Estudio/GitHub/Grafo01/notDirected.json");
-    if (ifs.fail()){cout<<"error"<<endl; exit(-1);}
-    json json1 =json::parse(ifs);
-    Graph<Airport>* graph = buildGraph(json1);
-    if (graph->findEdge(1,2))
-        cout<<"found"<<endl;
-    auto krusca=graph->Krusca();
-    if(graph->setIsConex()){
-        cout<<"conexo"<<endl;
-    }
-    if(graph->setIsBipar()){
-        cout<<"Es bipartito"<<endl;
-    }
-    if(graph->setIsNotDirected())
-        cout<<"IS not directed"<<endl;
-    graph->deleteNode(1);
 
+    Graph<Airport>* bipartiteGraph = readJsonAndReturnAirportGraph("bipartito.json");
+    Graph<Airport>* notDirectedGraph = readJsonAndReturnAirportGraph("notDirected.json");
+    Graph<Airport>* directedGraph = readJsonAndReturnAirportGraph("directed.json");
+    Graph<Airport>* connectedGraph = readJsonAndReturnAirportGraph("conexo.json");
+
+    if (notDirectedGraph->findEdge(1,2))
+        cout << "Edge found" << endl;
+
+    //auto krusca=notDirectedGraph->Krusca();
+
+    if (connectedGraph->setIsConnected())
+        cout << "The graph is connected" << endl;
+
+    if(bipartiteGraph->setIsBipartite())
+        cout << "The graph is bipartite" << endl;
+
+    if(notDirectedGraph->setIsNotDirected())
+        cout << "The graph is not directed" << endl;
+
+    if (directedGraph->setIsNotDirected())
+        cout << "The graph is directed" << endl;
+
+    return EXIT_SUCCESS;
 }
